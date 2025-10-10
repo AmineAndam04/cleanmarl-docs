562de71874532e1b26a4ef5d69f4ef44d583be3a
AndamAmine <andamamine83@gmail.com>
2025-10-10 22:17:15 +0100

    verif

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,14 +36,3 @@ html_theme = 'sphinx_rtd_theme'
 epub_show_urls = 'footnote'
 
 
-html_context = {
-    "metatags": '<meta name="google-site-verification" content="pGUJ3Uw-ACan4k_IFjAEHfonB-eSEdjpkTz0yX2cc34" />'
-}
-
-def setup(app):
-    app.add_html_theme('alabaster', None)  # or whatever theme you use
-    app.add_config_value('google_verification', '', 'html')
-    app.add_css_file('custom.css')  # optional
-
-    app.add_html_meta({'name': 'google-site-verification',
-                       'content': 'YOUR_VERIFICATION_CODE'})
